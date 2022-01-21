9b3bf6fac8c9b0b101821307f8f81a6cadbf1aa4
jrenatopontes <j.renato.pontes7@gmail.com>
2022-01-21 13:27:30 -0300

    inclusão app simples de cadastro em series

---
+++ DIO.Series/Enum/Genero.cs
@@ -0,0 +1,19 @@
+namespace DIO.Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        Comedia = 3,
+        Documentario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        Fantasia = 8,
+        Ficcao_Cientifica = 9,
+        musical = 10,
+        romance = 11,
+        suspense = 12,
+        terror = 13,    
+    }
+}
